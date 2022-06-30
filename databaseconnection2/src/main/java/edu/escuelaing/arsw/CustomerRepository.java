8c68f193ecd2d79aec0da749170965faca450fbe
unknown <edgar.henao@labinfo.is.escuelaing.edu.co>
2022-06-30 11:54:36 -0500

    first commit

---
+++ databaseconnection2/src/main/java/edu/escuelaing/arsw/CustomerRepository.java
@@ -0,0 +1,12 @@
+package edu.escuelaing.arsw;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
